3d4cd7ba8dc0d2a54cd25aa5f06247d127ec6bef
LyndaLilly <lilianlynda22@gmail.com>
2025-10-19 15:41:22 +0100

    updated

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::call(function () {
+    \App\Models\Promote::where('is_active', true)
+        ->where('end_date', '<', now())
+        ->update([
+            'is_active' => false,
+            'expired_at' => now(),
+        ]);
+})->daily();
