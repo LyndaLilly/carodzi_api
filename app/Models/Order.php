6848ddd417ea480e70bf138f68c85f6d7212c48b
LyndaLilly <lilianlynda22@gmail.com>
2025-10-31 16:34:46 +0100

    updated

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Buyer;
+use App\Models\Seller;
+use App\Models\ProductUpload;
 
 class Order extends Model
 {
