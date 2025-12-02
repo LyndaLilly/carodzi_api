b2335100e5f6beb1346dea07e27af0f7f01d9d06
LyndaLilly <lilianlynda22@gmail.com>
2025-12-02 13:37:55 +0100

    updated

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -99,9 +99,5 @@ class Seller extends Authenticatable
         return $this->hasMany(\App\Models\SellerProfileView::class, 'seller_id');
     }
 
-       public function notifications()
-    {
-        return $this->morphMany(DatabaseNotification::class, 'notifiable')->orderBy('created_at', 'desc');
-    }
 
 }
